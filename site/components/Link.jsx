9b1ec522ad4cef1e351e9d117cb8e5932e57542b
Andrzej Pacholik <andrzej.pacholik@unity.pl>
2020-06-06 16:40:18 +0200

    Ench(site): Add an improved Link (next) to be stylized

---
+++ site/components/Link.jsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+export const CustomLink = ({ children, className, ...resProps }) =>
+    (<Link {...resProps}><a className={className}>{children}</a></Link>);
+
+export const StyledLink = styled(CustomLink)`
+    color: ${({theme}) => theme.colors.normal };
+    text-decoration: none;
+`
