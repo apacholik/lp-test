505b03ad2c4ea253bdb8912a8584290c7c1a1171
Andrzej Pacholik <andrzej.pacholik@unity.pl>
2020-06-06 21:15:12 +0200

    Ench(site): Add about page

---
+++ site/pages/about.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>About Me</title>
+      </Head>
+      <article>
+        <Headers>Andrzej Pacholik</Headers>
+        <p>First, tested paragraph</p>
+      </article>
+    </>
+  )
+}
+
