eaaaa5bb5f88fa01de2b317d96699082878f5419
Andrzej Pacholik <andrzej.pacholik@unity.pl>
2020-06-07 13:38:49 +0200

    Fix(site): Add passHref props

--- site/components/Link.jsx
+++ site/components/Link.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 export const CustomLink = ({ children, className, ...resProps }) =>
-    (<Link {...resProps}><a className={className}>{children}</a></Link>);
+    (<Link {...resProps} passHref><a className={className}>{children}</a></Link>);
 
 export const StyledLink = styled(CustomLink)`
     color: ${({theme}) => theme.colors.normal };
